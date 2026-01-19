e9044ed4e6bc285118c7894d59932cdda663768e
hadeedhussainmemon <programmerhadeed@gmail.com>
2026-01-20 01:41:19 +0500

    feat: introduce location data and corresponding image assets for various locations.

--- src/lib/locations.ts
+++ src/lib/locations.ts
@@ -24,4 +24,22 @@ export const locations: Location[] = [
         description: "The cozy room where it all began. Check the desk drawer for a time machine!",
         image: "/locations/nobita-room.jpg",
     },
+    {
+        id: "future-city",
+        name: "22nd Century City",
+        description: "Doraemon's home time! Marvel at the flying cars and futuristic skyline of Tokyo in 2112.",
+        image: "/locations/future_city.png",
+    },
+    {
+        id: "cloud-kingdom",
+        name: "Cloud Kingdom",
+        description: "A magical castle in the sky built with the Cloud Hardening Gas. Don't fall off!",
+        image: "/locations/cloud_kingdom.png",
+    },
+    {
+        id: "school-hill",
+        name: "School Back Hill",
+        description: "The peaceful grassy hill where Nobita loves to take naps. A perfect spot for cloud gazing.",
+        image: "/locations/school_hill.png",
+    },
 ];
